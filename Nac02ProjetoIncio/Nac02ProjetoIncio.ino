fc55391e80e952c696e5df4fd25f9ad2765dc189
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-23 11:39:30 -0300

    Nac02 Arduino feito

--- Nac02ProjetoIncio/Nac02ProjetoIncio.ino
+++ Nac02ProjetoIncio/Nac02ProjetoIncio.ino
@@ -5,32 +5,51 @@
 DHT dht(DHTPIN, DHTTYPE); 
 
 
-int luz=A3;
+const int luz=A3;
 const int TAMANHO = 200;
-int rele=2;
+const int rele=2;
+const int led =6;
 
 void setup() {
    Serial.begin(9600);
    dht.begin();
    pinMode(rele,OUTPUT);
-
+   pinMode(led,OUTPUT);
 }
 
 void loop() {
 
 
 
-  /*if (Serial.available() > 0) {
+  if (Serial.available() > 0) {
     //Lê o texto disponível na porta serial:
    char texto[TAMANHO];
      Serial.readBytesUntil('\n', texto, TAMANHO);
      //Grava o texto recebido como JSON
      StaticJsonBuffer<TAMANHO> jsonBuffer;
      JsonObject& json = jsonBuffer.parseObject(texto);
-     if(json.success() && json.containsKey("led")) {
-       analogWrite(LED, json["led"]);
+     if(json.success()) {
+       if( json.containsKey("led")){
+           analogWrite(led, json["led"]);
+            delay(4000);
        }
- }*/
+      
+       
+       if( json.containsKey("rele")){
+          if(json["rele"]=true){
+             digitalWrite(rele,LOW);
+              delay(4000);
+           }
+           
+        if(json["rele"]=false){
+          digitalWrite(rele,HIGH);
+           delay(4000);
+           }
+       }
+
+       }
+
+ }
 
  
  StaticJsonBuffer<TAMANHO> jsonBuffer;
@@ -41,11 +60,13 @@ void loop() {
  json.printTo(Serial); Serial.println();
 
 
-if(json["luz"]<551){
-digitalWrite(rele,LOW);
- 
- delay(1000);
-}else{
-  digitalWrite(rele,HIGH);
-}
+  if(json["luz"]<550 && json["umidade"]>65){
+     digitalWrite(rele,LOW);
+     digitalWrite(led,HIGH); 
+    
+    }else{
+    digitalWrite(rele,HIGH);
+    digitalWrite(led,LOW); 
+   }
+    delay(1000);
 }
